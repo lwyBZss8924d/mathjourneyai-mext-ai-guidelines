5475d58aea3a7688beb1cb774c516feaad59daf6
lwyBZss8924d <sj112358@gmail.com>
2024-12-27 19:08:42 +0800

    fix: update next.config.js to use turbo.rules

--- next.config.js
+++ next.config.js
@@ -1,28 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['localhost'],
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**'
-      }
-    ]
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
-    return config;
-  },
   experimental: {
-    optimizeCss: true,
     turbo: {
-      loaders: {
-        '.svg': ['@svgr/webpack'],
-      },
+      rules: {
+        "*.mdx": ["mdx-loader"]
+      }
     },
+    optimizeCss: true
+  },
+  images: {
+    domains: ['localhost']
   }
 }
 
